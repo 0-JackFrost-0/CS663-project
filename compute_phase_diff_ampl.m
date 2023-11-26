59dbc5d871233727cc35ce461901de9db1001265
Archit Gupta <archit.gupta1006@gmail.com>
2023-11-26 22:25:37 +0530

    added operators

--- compute_phase_diff_ampl.m
+++ compute_phase_diff_ampl.m
@@ -1,5 +1,5 @@
 function [phase_diff_cos,phase_diff_sin, amp] = compute_phase_diff_ampl(c_r,c_x,c_y,p_r,p_x,p_y)
-    q_conj_prod_real = c_r.*p_r + c_x.*p_x + c_y.*p_y;
+    q_conj_prod_real = mull_cell(c_r,p_r) + mull_cell(c_x,p_x) + mull_cell(c_y,p_y);
     q_conj_prod_x = -c_r.*p_x + p_r.*c_x;
     q_conj_prod_y = -c_r.*p_y + p_r.*c_y;
     q_conj_amp = sqrt(q_conj_prod_real.^2 + q_conj_prod_y.^2 + q_conj_prod_x.^2);
---
+++ div_cell.m
@@ -0,0 +1,5 @@
+function div = mul_cell(a,b)
+    a_mat= zeros(size(a));
+    b_mat= zeros(size(b));
+    div = a_mat./b_mat;
+end
